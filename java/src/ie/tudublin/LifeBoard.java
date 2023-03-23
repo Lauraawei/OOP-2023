6d715d955228480b064445f5804672bdd1aedc7a
Lauraawei <C21371683@mytudublin.ie>
2023-03-23 11:16:33 +0000

    life

--- java/src/ie/tudublin/LifeBoard.java
+++ java/src/ie/tudublin/LifeBoard.java
@@ -1,16 +1,25 @@
+
 package ie.tudublin;
 
 import processing.core.PApplet;
 
 public class LifeBoard {
     boolean[][] board;
+    boolean[][] next;
     private int size;
     private PApplet p;
     float cellwidth;
 
     public boolean getcell(int row, int col)
     {
-        if (row >= 0 && row <= size -1 )
+        if (row >= 0 && row <= size -1 && col >= 0 && col <=size - 1)
+        {
+            return board[row][col];
+        }
+        else
+        {
+            return false;
+        }
     }
 
     public LifeBoard(int size, PApplet p)
@@ -35,29 +44,44 @@ public class LifeBoard {
 
     public int countCells(int row, int col)
     {
-        for (int i = -1; i <= 1; i++)
-        {
-            if (! (i == 0) && (j == 0))
-            {
-                if(getcell)
+        int count = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (!(i == 0 && j == 0) && row + i >= 0 && row + i < size && col + j >= 0 && col + j < size) {
+                    if (board[row + i][col + j]) {
+                        count++;
+                    }
+                }
             }
         }
+        return count;
     }
 
-    public void applyRules()
-    {
-        for(int row = 0; row < size; row ++)
-        {
-            for (int col = 0; col <size; col ++)
-            {
-                int count = countCells(row,col);
-                // < 2 > 3 dies
-                // 2-3 survives 
-                // dead with 3 neighbours come to life
-
+       
+                
+    public void applyRules(){
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                int count = countCells(row, col);
+                if (board[row][col]) {
+                    if (count < 2 || count > 3) {
+                        next[row][col] = false; // cell dies
+                    } else {
+                        next[row][col] = true; // cell survives
+                    }
+                } else {
+                    if (count == 3) {
+                        next[row][col] = true; // cell comes to life
+                    } else {
+                        next[row][col] = false; // cell remains dead
+                    }
+                }
             }
         }
-}
+        boolean[][] temp = board;
+        board = next;
+        next = temp;
+    }
 
     public int getSize() {
         return size;
@@ -66,5 +90,5 @@ public class LifeBoard {
     public void setSize(int size) {
         this.size = size;
     }
-    
 }
+    
